b0f29fb5d3359da806813b9f928cea937afc3854
Brandon Chao <34197234+didikid3@users.noreply.github.com>
2025-11-22 17:36:11 -0500

    adding full test script:

---
+++ run_full.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=prompt_engineering_full
+#SBATCH --account=eecs595f25_class
+#SBATCH --partition=spgpu
+#SBATCH --gpus=1
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=16g
+#SBATCH --mail-user=bchao@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=validate-full.out
+
+python score_essays.py \
+    --input-file train_dev.csv \
+    --output-file results.jsonl \
+    --evaluate
